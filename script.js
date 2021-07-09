3e7e72664ec24c598ed54599db4f11f82da04ce1
Sungmin Ashley Ro <sungminro92@gmail.com>
2021-07-09 15:17:26 -0400

    words list results working

--- script.js
+++ script.js
@@ -3,9 +3,97 @@ window.onload = function() {
     console.log("it's working");
     // console.log(test.definitions + test.synonyms);
 
-    console.log(WordData.words);
+    getWords("success");
+    // console.log(WordData.words);
 };
+const searchButton = document.getElementById("search-button");
+const input = document.getElementById("input-word");
+const resultContainer = document.getElementById("result-container");
+const clickedWordsContainer = document.getElementById("saved-words-container");
+const errorMessage = document.getElementById("error-message");
+const wordAPI = "https://wordsapiv1.p.rapidapi.com/words/";
 
+if (searchButton) {
+    searchButton.addEventListener("click", function() {
+      getSynonyms(getWords(input.value));
+    })
+}
+
+if (input) {
+    input.addEventListener("keyup", keyPress);
+}
+
+// PRESS ENTER AS TO CLICK SEARCH BUTTON
+function keyPress(event) {
+    if (event.keyCode === 13) {
+        if (event.value !== undefined ) {
+          input.value = event.value;
+        }
+        getWords(input.value);
+    }
+}
+
+function clearError() {
+  errorMessage.innerHTML = "";
+}
+
+function getWords(inputValue) {
+  clearError();
+  resultContainer.innerHTML = "";
+  fetch(wordAPI + inputValue, {
+    headers: {"X-Mashape-Key": "267a89e0c1msh4a664cb7046de60p1abc82jsna3d384a0934e"}
+  })
+  .then((resp) => resp.json())
+  .then(function(data) {
+    if(!data.success) {
+      // console.log(data.message);
+      if(data.message === undefined) {
+
+      } else {errorMessage.innerHTML = data.message;}
+    }
+    let synonymsResults = data.results
+      .map(result => result.synonyms)
+      .filter(synonym => synonym)
+      .flatMap(synonym => synonym);
+    console.log(synonymsResults);
+    for(let i=0; i<synonymsResults.length; i++) {
+        let wordValue = synonymsResults[i];
+        let wordDiv = document.createElement('div');
+        wordDiv.innerHTML = wordValue;
+        wordDiv.setAttribute("class", "synonyms");
+        wordDiv.addEventListener('click', function() {
+          input.value = wordValue;
+          clikedWords(wordValue);
+          getWords();
+        });
+        resultContainer.appendChild(wordDiv);
+    }
+  })
+
+}
+
+function getSynonyms(data) {
+  console.log(data);
+  // let synonymsResult = data.results
+  // .map(result => result.synonyms)
+  // .filter(synonym => synonym)
+  // .flatMap(synonym => synonym);
+  // console.log(synonymsResult);
+}
+
+function clikedWords(word) {
+    let clickedWordDiv = document.createElement('div');
+    // let x = document.createElement('a');
+    // x.innerHTML ="  x";
+    // x.style.color = "red";
+    // x.style.padding = "2px 3px 2px 3px";
+    clickedWordDiv.innerHTML = word;
+    // clickedWordDiv.appendChild(x);
+    clickedWordsContainer.appendChild(clickedWordDiv);
+    clickedWordDiv.setAttribute("class", "words");
+    clickedWordDiv.style.flexDirection = "column";
+    x.addEventListener('click', remove, false);
+}
 
 const WordData = {
   words: [],
@@ -13,14 +101,15 @@ const WordData = {
   synonyms: false,
   antonyms: false,
 
+// SAVING WORD INTO WORDS DATA
   assignWord: function(definitions, partOfSpeech, synonyms, antonyms, examples) {
     this.words.push(new Word(definitions, partOfSpeech, synonyms, antonyms, examples));
   }
 }
 
-
-WordData.assignWord("this","thisss","th","thd","thids");
-WordData.assignWord("thias","thissss","tfh","thddd","thsssds");
+function displayResults() {
+  getWords();
+}
 
 function Word(definitions, partOfSpeech, synonyms, antonyms, examples) {
   this.definitions = definitions;
